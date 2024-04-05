c0a5f5011d2cfcb34844269ee8a48dd3c678bf17
Husnainn01 <husnain@ss-japan.com>
2024-04-05 19:46:16 +0900

    Deleted Becuase it was old code

--- resources/views/admin/freights/show.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('admin.app_admin')
-@section('admin_content')
-    <h1 class="h3 mb-3 text-gray-800">Insurance</h1>
-@endsection
--- resources/views/front/customerDashboard/customer_footer.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="footer">
-    <div class="copyright">
-       <p>Copyright © SS Japan<a href="" target="_blank">Limited</a> 2023</p>
-    </div>
-</div>
